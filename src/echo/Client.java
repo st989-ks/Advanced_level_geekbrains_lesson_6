53e7b4902b41dfd48073cfa9cbd7325957c733e1
Stanislav <st989@ya.ru>
2020-10-09 23:37:54 +0400

    Решение к заданию урока №6

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -1,5 +1,59 @@
 package echo;
 
-public class Client {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
+import static echo.Server.PORT;
+
+public class Client {
+    public static void main(String[] args) {
+        Socket socket = null;
+
+        try {
+            socket = new Socket( "localhost", PORT );
+            System.out.println( "Client connected" );
+            Scanner in = new Scanner( socket.getInputStream() );
+            PrintWriter out = new PrintWriter( socket.getOutputStream(), true );
+            Scanner console = new Scanner( System.in );
+
+            Thread t1 = new Thread( new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String str = in.nextLine();
+
+                        if (str.equals( "/end" )) {
+                            break;
+                        }
+                        System.out.println( "Server: " + str );
+                    }
+                }
+            } );
+            t1.start();
+
+            Thread t2 = new Thread( new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String str = console.nextLine();
+                        out.println( str );
+                        System.out.println( "Client: " + str );
+                    }
+                }
+            } );
+            t2.setDaemon( true );
+            t2.start();
+
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/echo/Server.java
+++ src/echo/Server.java
@@ -7,44 +7,60 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
+    final static int PORT = 3000;
+
     public static void main(String[] args) {
         ServerSocket server = null;
         Socket socket = null;
-        final int PORT = 8189;
 
         try {
-            server = new ServerSocket(PORT);
-            System.out.println("Server started");
+            server = new ServerSocket( PORT );
+            System.out.println( "Server started" );
             socket = server.accept();
-            System.out.println("Client connected");
+            System.out.println( "Client connected" );
 
-            Scanner in = new Scanner(socket.getInputStream());
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            Scanner in = new Scanner( socket.getInputStream() );
+            PrintWriter out = new PrintWriter( socket.getOutputStream(), true );
+            Scanner console = new Scanner( System.in );
 
-            while (true) {
-                String str = in.nextLine();
+            Thread t1 = new Thread( new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String str = in.nextLine();
 
-                if(str.equals("/end")){
-                    break;
+                        if (str.equals( "/end" )) {
+                            break;
+                        }
+
+                        System.out.println( "Client: " + str );
+                    }
                 }
+            } );
+            t1.start();
 
-                System.out.println("Client: " + str);
-                out.println("echo: "+ str);
+            Thread t2 = new Thread( new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String str = console.nextLine();
+                        out.println( str );
+                        System.out.println( "Server: " + str );
+                    }
+                }
+            } );
+            t2.setDaemon( true );
+            t2.start();
+
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                server.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
+
